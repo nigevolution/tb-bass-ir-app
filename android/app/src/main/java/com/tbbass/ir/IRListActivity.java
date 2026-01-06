0e5a264a3c5d548c9b5102f9a94830b4f55f242e
ELBS <86670001+nigevolution@users.noreply.github.com>
2026-01-06 12:03:14 -0400

    Update IRListActivity.java

--- android/app/src/main/java/com/tbbass/ir/IRListActivity.java
+++ android/app/src/main/java/com/tbbass/ir/IRListActivity.java
@@ -4,16 +4,39 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class IRListActivity extends Activity {
+
+    String[] irs = {
+            "BASS MODS",
+            "FENDER ULTRA",
+            "MAYONES JARBA 5",
+            "MUSIC MAN",
+            "SADOWSKY M5",
+            "SADOWSKY METROLINE",
+            "SMITH BASS",
+            "WARWICK PROSERIES"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView wv = new WebView(this);
-        wv.loadUrl("https://tb-ir-bass.netlify.app/client.html");
-        setContentView(wv);
+        ListView list = new ListView(this);
+        setContentView(list);
+
+        list.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, irs));
+
+        list.setOnItemClickListener((a, b, i, l) -> {
+            String ir = irs[i];
+
+            // ABRE O M-EFFECTS COM O IR
+            Intent open = new Intent(Intent.ACTION_VIEW);
+            open.setData(Uri.parse("mfx://import/" + ir.replace(" ", "_")));
+            startActivity(open);
+        });
     }
 }
